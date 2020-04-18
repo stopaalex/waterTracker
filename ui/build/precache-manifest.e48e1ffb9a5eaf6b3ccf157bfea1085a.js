86b7817589d185017b9b800fa4df7f9b5d59f2ba
stopaalex <stopa.alex@gmail.com>
2020-04-18 19:25:00 -0400

    UPDATE ::: pushing the build to github too

---
+++ ui/build/precache-manifest.e48e1ffb9a5eaf6b3ccf157bfea1085a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "657048fdc40280b4b5d93ad8d04c006e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31f66ce6ed74e1cd56aa",
+    "url": "/static/css/main.40556c6e.chunk.css"
+  },
+  {
+    "revision": "905685f0903509e542ff",
+    "url": "/static/js/2.18809656.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.18809656.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31f66ce6ed74e1cd56aa",
+    "url": "/static/js/main.9bbc8880.chunk.js"
+  },
+  {
+    "revision": "13920b6a76ec4f7e0b8b",
+    "url": "/static/js/runtime-main.db9d15da.js"
+  }
+]);
